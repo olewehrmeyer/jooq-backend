0a2c296757754e0173bce0d50a4d0a742ed43d59
Yann Massard <yamass@gmail.com>
2017-05-05 10:06:25 +0200

    copied jooq example into project

---
+++ src/main/java/org/jooq/example/spring/Application.java
@@ -0,0 +1,16 @@
+package org.jooq.example.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Thomas Darimont
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
