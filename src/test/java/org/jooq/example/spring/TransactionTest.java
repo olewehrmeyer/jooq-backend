0150e65fb310b1ff7d70fa2c87332f06ce4b032a
Yann Massard <yamass@gmail.com>
2017-05-05 11:12:21 +0200

    postgres

--- src/test/java/org/jooq/example/spring/TransactionTest.java
+++ src/test/java/org/jooq/example/spring/TransactionTest.java
@@ -1,6 +1,6 @@
 package org.jooq.example.spring;
 
-import static org.jooq.example.db.h2.Tables.BOOK;
+import static org.jooq.example.db.Tables.BOOK;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
